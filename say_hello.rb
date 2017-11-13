f6e7d459f95b979b6e0505d3b4be85b0b4f5dc3a
Christopher Lam <christopher.lam@students.mq.edu.au>
2017-11-13 22:43:43 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,12 @@
 # Build your say_hello method here
+# I call on the method, say_hello, and give it the string "Gabriela"
+
+def say_hello(name = "world!")
+  puts "Hello, #{name}"
+end
+
+say_hello
+
+say_hello("Gabriela")
+
+# The method prints this text to the screen:
