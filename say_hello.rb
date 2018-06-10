4ac4969c8c034dddc20ea41b1d12d53f61d824f8
Jackie Honikman <impresaria@argsr.us>
2018-06-10 02:27:14 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,7 @@
-# Build your say_hello method here
+# I call on the method, say_hello, and give it the string "Gabriela"
+def say_hello
+  say_hello("Gabriela")
+end
+
+# The method prints this text to the screen:
+Hello Gabriela!
