cd26695b46340c4dc9d9ac9e56a71cd4805c9d5b
Vishal Wavhale <cden7017@gmail.com>
2018-12-27 17:51:59 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,10 @@
 # Build your say_hello method here
+def say_hello(name)
+  if(name != "")
+    print("Hello "+name+"!");
+  else
+    print("Hello Ruby Programmer");
+end
+
+say_hello("Vishal")
+say_hello()
